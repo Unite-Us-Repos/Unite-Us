5edde9fb214306febed971c4ae2f915531e08f6f
Sabrina Matthews <sm@ggalmightydigital.com>
2024-06-18 00:43:03 -0400

    updating networks pop up colors

--- wp-content/themes/uniteus-sage/resources/views/components/code-editor/default.blade.php
+++ wp-content/themes/uniteus-sage/resources/views/components/code-editor/default.blade.php
@@ -129,7 +129,7 @@ const instance = tippy('#us-map path[data-info]', {
 </script>
 <div style="display: none;position:relative;">
   <div id="stateTemplate"><a href="#" id="close-bubble" class="close-bubble"><svg xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6" fill="#4d95cf" viewBox="0 0 24 24" stroke="white" stroke-width="2">
+        class="h-6 w-6" fill="#0B1538" viewBox="0 0 24 24" stroke="white" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round"
           d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg></a>
