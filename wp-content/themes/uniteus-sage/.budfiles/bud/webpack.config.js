ea6af1f0519626253d5a1a7b0c78b652cd5e4e00
Sabrina Matthews <sm@ggalmightydigital.com>
2024-06-22 16:44:16 -0400

    test

--- wp-content/themes/uniteus-sage/.budfiles/bud/webpack.config.js
+++ wp-content/themes/uniteus-sage/.budfiles/bud/webpack.config.js
@@ -346,6 +346,22 @@ module.exports = {
       }
     },
     {
+      "name": "WordPressExternalsWebpackPlugin",
+      "stage": null,
+      "externals": {
+        "type": "window"
+      }
+    },
+    {
+      "plugin": {
+        "name": "WordPressDependenciesWebpackPlugin",
+        "stage": null
+      },
+      "manifest": {},
+      "usedDependencies": {},
+      "fileName": "wordpress.json"
+    },
+    {
       "options": {
         "emitHtml": false,
         "publicPath": ""
@@ -357,28 +373,12 @@ module.exports = {
       "name": "entrypoints.json"
     },
     {
-      "name": "WordPressExternalsWebpackPlugin",
-      "stage": null,
-      "externals": {
-        "type": "window"
-      }
-    },
-    {
       "plugin": {
         "name": "MergedManifestPlugin"
       },
       "file": "entrypoints.json",
       "entrypointsName": "entrypoints.json",
       "wordpressName": "wordpress.json"
-    },
-    {
-      "plugin": {
-        "name": "WordPressDependenciesWebpackPlugin",
-        "stage": null
-      },
-      "manifest": {},
-      "usedDependencies": {},
-      "fileName": "wordpress.json"
     }
   ],
   "entry": {
