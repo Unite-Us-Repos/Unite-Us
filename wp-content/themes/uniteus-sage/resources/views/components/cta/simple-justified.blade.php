2e6843e462dc2a530cc0fbed62177b4c9313fef8
Sabrina Matthews <sm@ggalmightydigital.com>
2024-06-22 00:51:18 -0400

    updating color changes QA

--- wp-content/themes/uniteus-sage/resources/views/components/cta/simple-justified.blade.php
+++ wp-content/themes/uniteus-sage/resources/views/components/cta/simple-justified.blade.php
@@ -1,4 +1,12 @@
 <section @isset ($section['id']) id="{{ $section['id'] }}" @endisset class="component-section {{ $section_classes }}">
+  <div class="absolute inset-0">
+    @if ($background['image'])
+      <img fetchpriority="high" class="w-full h-full object-cover @if ('top' == $background['position']) object-top @endif @if ('bottom' == $background['position']) object-bottom @endif" src="{{ $background['image']['sizes']['medium'] }}"
+        srcset="{{ $background['image']['sizes']['medium'] }} 300w, {{ $background['image']['sizes']['2048x2048'] }} 1024w"
+        sizes="(max-width: 600px) 300px, 1024px"
+        alt="{{ $background['image']['alt'] }}">
+    @endif
+  </div>
   <div class="component-inner-section @if ('dark' == $background['color']) is-dark-bg text-white @endif">
     <div class="md:grid md:grid-cols-12 gap-10">
       <div class="md:col-span-5">
