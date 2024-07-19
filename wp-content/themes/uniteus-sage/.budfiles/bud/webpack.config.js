85389e00ee61c556b71bb8212ca5f843a525611e
Sabrina Matthews <sm@ggalmightydigital.com>
2024-07-19 12:18:34 -0400

    fixing sections

--- wp-content/themes/uniteus-sage/.budfiles/bud/webpack.config.js
+++ wp-content/themes/uniteus-sage/.budfiles/bud/webpack.config.js
@@ -346,6 +346,13 @@ module.exports = {
       }
     },
     {
+      "name": "WordPressExternalsWebpackPlugin",
+      "stage": null,
+      "externals": {
+        "type": "window"
+      }
+    },
+    {
       "options": {
         "emitHtml": false,
         "publicPath": ""
@@ -358,27 +365,20 @@ module.exports = {
     },
     {
       "plugin": {
+        "name": "MergedManifestPlugin"
+      },
+      "file": "entrypoints.json",
+      "entrypointsName": "entrypoints.json",
+      "wordpressName": "wordpress.json"
+    },
+    {
+      "plugin": {
         "name": "WordPressDependenciesWebpackPlugin",
         "stage": null
       },
       "manifest": {},
       "usedDependencies": {},
       "fileName": "wordpress.json"
-    },
-    {
-      "name": "WordPressExternalsWebpackPlugin",
-      "stage": null,
-      "externals": {
-        "type": "window"
-      }
-    },
-    {
-      "plugin": {
-        "name": "MergedManifestPlugin"
-      },
-      "file": "entrypoints.json",
-      "entrypointsName": "entrypoints.json",
-      "wordpressName": "wordpress.json"
     }
   ],
   "entry": {
