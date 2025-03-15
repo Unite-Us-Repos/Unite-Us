d85bda1891225bc183622e4561ebf00228f7bb7f
josetamayo <josejtamayo@gmail.com>
2025-03-14 18:03:10 -0700

    form bolded text conditional

--- wp-content/themes/uniteus-sage/resources/views/components/content/two-columns-with-form.blade.php
+++ wp-content/themes/uniteus-sage/resources/views/components/content/two-columns-with-form.blade.php
@@ -8,7 +8,7 @@
   @endphp
   @php
   $case_type = $section['case_type'] ?? 'Uppercase';
-  
+
   // Define case type classes
   $case_class = '';
   switch ($case_type) {
@@ -29,7 +29,7 @@
   @if ($background['has_divider'])
     @includeIf('dividers.waves')
   @endif
-  
+
   <section @isset ($section['id']) id="{{ $section['id'] }}" @endisset class="relative component-section {{ $section_classes }} @if ($section_settings['collapse_padding']) {{ $section_settings['padding_class'] }} @endif">
   @if ($background['image'])
       <div class="absolute inset-0">
@@ -39,21 +39,21 @@
           alt="{{ $background['image']['alt'] }}">
       </div>
     @endif
-  
-  
+
+
     @if ($background['overlay'])
     <div class="absolute inset-0 bg-brand opacity-90"></div>
     @endif
     <div class="component-inner-section @if ($section_settings['fullscreen']) fullscreen @endif @if ($background['divider_bottom']) pb-10 @endif">
-  
+
       <div class="relative z-10 md:relative flex flex-col lg:grid lg:grid-cols-2 gap-10 lg:gap-40">
-  
+
         <div class="@if ($background['color'] == 'dark') text-white @endif lg:col-span-1 @if ('center' == $vertical_alignment) flex flex-col justify-center @endif text-lg">
           @if ($section['subtitle'])
             @if ($section['subtitle_display_as_pill'])
               <span class="grow-0 @if ($background['color'] == 'dark') bg-white bg-opacity-10 text-action-light-blue @else text-action @if ($background['color'] == 'light-gradient') bg-white @else bg-light mix-blend-multiply @endif @endif text-sm py-1 px-4 inline-block mb-6 rounded-full {{ $case_class }}">
             @else
-              <span class="subtitle pb-4 {{ $case_class }}">
+              <span class="subtitle block pb-4 {{ $case_class }}">
             @endif
               {!! $section['subtitle'] !!}
               </span>
@@ -62,7 +62,7 @@
             <img class="mb-6 max-w-[224px] h-auto" src="{{ $section['logo']['sizes']['medium'] }}" alt="{{ $section['logo']['alt'] }}" />
           @endisset
           @if ($section['title'])
-          
+
           @if ($section['is_header'] === 'h1')
           <h1 class="hfont-semibold mb-6 width-28 h2">{!! $section['title'] !!}</h1>
           @elseif ($section['is_header'] === 'h2')
@@ -72,13 +72,13 @@
           @endif
 
           @endif
-  
+
           @if ($section['description'])
             <div class="description">
               {!! $section['description'] !!}
             </div>
           @endif
-  
+
           @isset ($extra_content)
             @if ('testimonial' == $extra_content)
               @if ($testimonial['quote'])
@@ -90,10 +90,10 @@
                 </blockquote>
               @endif
             @endif
-  
+
             @if ('mini-cards' == $extra_content)
               @if ($mini_cards)
-                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-10 gap-3">
+                <div class="grid grid-cols-1 sm:grid-cols-2 mt-10 gap-3">
                   @foreach ($mini_cards as $card)
                     <div class="relative text-center sm:text-left border border-blue-200 rounded-xl p-6 group">
                       <div class="relative z-10">
@@ -101,7 +101,7 @@
                       <div>{!! $card['description'] !!}</div>
                       </div>
                       <div class="absolute inset-0 mini-card-grad z-0 rounded-xl opacity-0 group-hover:opacity-100">
-  
+
                       </div>
                     </div>
                   @endforeach
@@ -109,7 +109,7 @@
               @endif
             @endif
           @endisset
-  
+
           @if ($buttons)
             @php
               $data = [
@@ -120,7 +120,7 @@
           @endif
         </div>
         <div class="flex flex-col lg:col-span-1 @if ('center' == $vertical_alignment) justify-center @endif relative">
-  
+
           @if ($mask_image)
           <div class="relative max-w-lg mx-auto" @if ($featured_image) style="background: url({{ $featured_image['sizes']['medium_large'] }}) no-repeat center center;background-size: 60%;" @endif>
             <img class="lazy" data-src="@asset('/images/network-mask-1.png')" alt="" />
@@ -130,7 +130,7 @@
               <img class="lazy rounded-lg w-full @if ($set_max_width_height) h-full aspect-square object-contain max-h-96 max-w-sm mx-auto @else max-w-md mx-auto lg:max-w-3xl @endif" data-src="@if ('image/gif' == wp_get_image_mime($featured_image['url'])) {{ $featured_image['url'] }} @else {{ $featured_image['sizes']['medium_large'] }} @endif" alt="{{ $featured_image['alt'] }}" />
             @endisset
           @endif
-  
+
           @isset ($embeds)
             @if (!empty($embeds))
               <div class="rounded-lg responsive-embed">
@@ -138,7 +138,7 @@
               </div>
             @endif
           @endisset
-  
+
           @isset ($code_editor)
             @if (!empty($code_editor))
               <div>
@@ -146,12 +146,17 @@
               </div>
             @endif
           @endisset
-  
+
           @isset ($form_code)
             @if (!empty($form_code))
               <div id="formIframe" class="rounded-md p-6 sm:p-10 bg-light-gradient shadow-md">
                 <div class="flex justify-between items-center mb-4">
-                  <h3 class="text-xl mb-0 font-medium capitalize"><span class="font-bold">{{ $form_bolded_title }}</span>{{ $form_title }}</h3>
+                  <h3 class="text-xl mb-0 font-medium capitalize">
+                    @if ($form_bolded_title)
+                      <span class="font-bold">{{ $form_bolded_title }} </span>
+                    @endif
+                    {{ $form_title }}
+                  </h3>
                   <svg xmlns="http://www.w3.org/2000/svg" width="32" height="33" viewBox="0 0 32 33" fill="none">
                     <path d="M12.0003 16.3018H20.0003M12.0003 21.6351H20.0003M22.667 28.3018H9.33366C7.8609 28.3018 6.66699 27.1079 6.66699 25.6351V6.96842C6.66699 5.49567 7.8609 4.30176 9.33366 4.30176H16.7814C17.135 4.30176 17.4741 4.44223 17.7242 4.69228L24.9431 11.9112C25.1932 12.1613 25.3337 12.5004 25.3337 12.854V25.6351C25.3337 27.1078 24.1398 28.3018 22.667 28.3018Z" stroke="#C7D8E8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
@@ -163,8 +168,8 @@
               </div>
             @endif
           @endisset
-  
-  
+
+
         </div>
       </div>
     </div>
