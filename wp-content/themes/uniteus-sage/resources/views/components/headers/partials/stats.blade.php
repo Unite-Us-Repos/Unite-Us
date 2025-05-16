86979c987fe527524a313cb914199baa3383ba30
josetamayo <josejtamayo@gmail.com>
2025-05-15 20:50:26 -0700

    refactor: enhance stats component layout with vertical dividers and improved padding

--- wp-content/themes/uniteus-sage/resources/views/components/headers/partials/stats.blade.php
+++ wp-content/themes/uniteus-sage/resources/views/components/headers/partials/stats.blade.php
@@ -1,9 +1,9 @@
 @if ($stats)
 <div class="component-inner-section mb-10">
-  <div class="flex flex-col sm:grid grid-cols-{{ count($stats) }} px-4 gap-6">
+  <div class="vertical-dividers flex flex-col sm:grid grid-cols-{{ count($stats) }}">
     @foreach ($stats as $stat)
-      <div class="flex flex-col justify-between px-4 gap-6">
-        <div class="text-brand">{!! $stat['description'] !!}</div>
+      <div class="flex flex-col justify-between px-9 gap-6">
+        <div class="text-brand text-lg">{!! $stat['description'] !!}</div>
         @if ($stat['logo'])
           <img class="max-w-[120px] w-full h-auto mb-2" src="{{ $stat['logo']['sizes']['medium'] }}" alt="{{ $stat['logo']['alt'] }}" />
         @endif
@@ -12,3 +12,18 @@
   </div>
 </div>
 @endif
+<style>
+  /* Add vertical dividers between items (except first and last) */
+    .vertical-dividers div:not(:first-child)::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      width: 1px;
+      background-color: rgba(44, 64, 90, 0.2);
+    }
+    .vertical-dividers div {
+      position: relative;
+    }
+    </style>
