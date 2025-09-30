3ef2670509b2a47ef0ef6ff925113588ee15e410
Sabrina Matthews <sm@ggalmightydigital.com>
2025-09-29 21:30:11 -0400

    fixing KH

--- wp-content/themes/uniteus-sage/resources/views/components/grid-cards/partials/key-points.blade.php
+++ wp-content/themes/uniteus-sage/resources/views/components/grid-cards/partials/key-points.blade.php
@@ -2,7 +2,7 @@
 <div class="component-inner-section mb-10">
   <div class="flex flex-col gap-0 lg:gap-8 @if (count($key_points) > 1) md:grid md:grid-cols-2 lg:grid-cols-{{ count($key_points) }} @endif">
     @foreach ($key_points as $point)
-      <div class="flex top-gradient-line flex-col justify-between gap-8 p-8 lg:p-"0>
+      <div class="flex top-gradient-line flex-col justify-between gap-8 p-8 lg:p-0">
         <div class="text-brand text-lg">{!! $point['description'] !!}</div>
         @isset ($point['logo'])
           <span class="image-filter brand">
